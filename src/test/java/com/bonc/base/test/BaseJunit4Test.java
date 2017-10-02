d02f099809cae19ac025f63724d957f2e2cfbfa2
liyongqiang1 <liyongqiang1@bonc.com.cn>
2017-10-02 09:56:42 +0800

    test gitcommit

--- src/test/java/com/bonc/base/test/BaseJunit4Test.java
+++ src/test/java/com/bonc/base/test/BaseJunit4Test.java
@@ -7,5 +7,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class) // 使用junit4进行测试
 @ContextConfiguration({"classpath*:spring.xml" }) /// 加载配置文件
 public class BaseJunit4Test {
-	
+	//Helloween
 }
